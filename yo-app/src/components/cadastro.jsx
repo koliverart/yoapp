f36ede10bd45f4f25cbbd1976b8952c51d168979
Kelven <kelvenboss123@gmail.com>
2021-12-13 14:53:05 -0300

    Commit 0.2
    - Cadastro atualizado e já reconhecível.

--- yo-app/src/components/cadastro.jsx
+++ yo-app/src/components/cadastro.jsx
@@ -4,15 +4,29 @@ import './styles/cadastro.css';
 const cadastro = () => {
     return (
         <div>
-            <div id='login'>
-                <h1>YOAPP!</h1>
-                <div id='inputs'>
-                    <input type="text" placeholder='sexo2@transa.com'/>
-                    <input type="text" placeholder='Password'/>
+            <div id='cadastro'> 
+            <h1>YOAPP!</h1>
+                <div id='login'>
+                    <div id='inputs'>
+                        <input class="input1" type="text" placeholder='sexo2@transa.com'/>
+                        <input class="input1" type="text" placeholder='Password'/>
+                        <button>LOGIN</button>
+                        <p href="">Forgot your Password?</p>
+                        <div id='btn1'>X</div>
+                    </div>
+                
                 </div>
-              
+
+                <div id='singup'>
+                    <div id='inputs'>
+                        <input class="input2" type="text" placeholder='sexo2@transa.com'/>
+                        <input class="input2" type="text" placeholder='Password'/>
+                        <button>SING UP</button>
+                        <div id='btn2'>X</div>
+                    </div>
+                </div>
             </div>
-           
+
         </div>
     )
 }
