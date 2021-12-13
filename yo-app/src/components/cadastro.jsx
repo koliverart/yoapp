b9ae0cb1d57f659ae35817bbd78d2f156847c625
Kelven <kelvenboss123@gmail.com>
2021-12-13 13:05:51 -0300

    Commit 0.1
    Criação dos arquivos Front-End em React.

---
+++ yo-app/src/components/cadastro.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/cadastro.css';
+
+const cadastro = () => {
+    return (
+        <div>
+            <div id='login'>
+                <h1>YOAPP!</h1>
+                <div id='inputs'>
+                    <input type="text" placeholder='sexo2@transa.com'/>
+                    <input type="text" placeholder='Password'/>
+                </div>
+              
+            </div>
+           
+        </div>
+    )
+}
+
+export default cadastro;
