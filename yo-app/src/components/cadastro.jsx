8b21a02bade2f4353475977eaf2913539d4b65e0
Kelven <kelvenboss123@gmail.com>
2021-12-15 11:19:26 -0300

    Commit 0.3
    - Atualização da fonte

--- yo-app/src/components/cadastro.jsx
+++ yo-app/src/components/cadastro.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './styles/cadastro.css';
+import PropTypes from 'prop-types';
+//Jarvascript
+let btn1 = document.getElementById("btn1");
 
-const cadastro = () => {
+const cadastro = ({ extend1, extend2 }) => {
     return (
         <div>
             <div id='cadastro'> 
@@ -12,9 +15,13 @@ const cadastro = () => {
                         <input class="input1" type="text" placeholder='Password'/>
                         <button>LOGIN</button>
                         <p href="">Forgot your Password?</p>
-                        <div id='btn1'>X</div>
                     </div>
-                
+                </div>
+
+
+                <div id='bolas'>
+                    <div id='btn1' onClick={extend1}>▲</div>
+                    <div id='btn2' onClick={extend2}>▼</div>
                 </div>
 
                 <div id='singup'>
@@ -22,13 +29,26 @@ const cadastro = () => {
                         <input class="input2" type="text" placeholder='sexo2@transa.com'/>
                         <input class="input2" type="text" placeholder='Password'/>
                         <button>SING UP</button>
-                        <div id='btn2'>X</div>
                     </div>
                 </div>
             </div>
 
         </div>
     )
+}
+
+cadastro.defaultProps = {
+    extend1: ()=> {
+        btn1.style.display = 'none';
+    },
+    extend2: ()=> {
+        alert("hello");
+    },
+}
+
+cadastro.propTypes = {
+    extend1: PropTypes.func, 
+    extend2: PropTypes.func,
 }
 
 export default cadastro;
